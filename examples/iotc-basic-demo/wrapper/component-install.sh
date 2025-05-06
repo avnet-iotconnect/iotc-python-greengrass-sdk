1325b837420def81ef52fee44775c509550dbd7b
nik-markovic <nikola.m@gmail.com>
2025-05-06 14:41:25 -0500

    MP1 deployment patch

--- examples/iotc-basic-demo/wrapper/component-install.sh
+++ examples/iotc-basic-demo/wrapper/component-install.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 set -e
 set -x
 
 cd "$(dirname "$0")"/..
+
 
 python3 -m venv ~/.venv-basic-demo
 
@@ -21,6 +24,18 @@ if [[ -d local-packages ]]; then
     python3 -m pip uninstall -y iotconnect-greengrass-sdk || true
   fi
   python3 -m pip install --upgrade --force-reinstall ./local-packages/*.whl
+fi
+
+if grep OpenSTLinux /etc/issue > /dev/null && [[ $(uname -m) == armv7l ]]; then
+  # Special setup for OpenSTLinux on MP1 where pre-compiled package for awscrt is not available on pip
+  # Normally pip would compile those from source, but the build environment does not have everything needed to compile those
+  mkdir -p ~/tmp-wheels
+  awscrt_whl=awscrt-0.24.1-cp311-abi3-manylinux_2_28_armv7l.manylinux_2_31_armv7l.whl
+  awsiotsdk_whl=awsiotsdk-1.22.2-py3-none-any.whl
+  wget -q https://downloads.iotconnect.io/sdk/python/arm7l/$awscrt_whl -O ~/tmp-wheels/$awscrt_whl
+  wget -q https://downloads.iotconnect.io/sdk/python/arm7l/$awsiotsdk_whl -O ~/tmp-wheels/$awsiotsdk_whl
+  python3 -m pip install ~/tmp-wheels/$awscrt_whl ~/tmp-wheels/$awsiotsdk_whl
+  rm -rf ~/tmp-wheels
 fi
 
 python3 -m pip install -r requirements.txt
